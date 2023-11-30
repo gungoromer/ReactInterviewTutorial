f057180c92da34b61eafb694577859aca52899f0
OYENAL <OYENAL@DESKTOP-9PDS8TP>
2023-11-30 22:55:50 +0300

    onclick event passed var

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import Navbar from './components/NavBar';
-
+import User
+ from './components/User';
 import './App.css';
 
 function App() {
   return (
     <div className='container'>
       <Navbar title="Navbar title"/>
+      <User name="Ömer GÜNGÖR" phone="5327326949" mail="gungoromer@windowslive.com"/>
+      <User name="Esra GÜNGÖR" phone="5079374795" mail="esra.i@windowslive.com"/>
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class User extends Component {
-  render() {
-    return (
-      <div>
-        <form>
-            <input type='text'></input>
-            <button>Gönder</button>
-        </form>
-      </div>
-    )
-  }
+    state = {
+        isVisible: false
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+    onClickEvent(number, e) {
+        console.log(number);
+    }
+
+    render() {
+
+        const { name, mail, phone } = this.props;
+        const { isVisible } = this.state;
+
+        return (
+            <div className='card mb-5'>
+                <div className='card-header' onClick={this.onClickEvent.bind(this, 34)}>
+                    {name}
+                </div>
+                {
+                    isVisible ?
+                        <div className='card-body'>
+                            <p>{mail}</p>
+                            <p>{phone}</p>
+                            <p>{this.state.test}</p>
+                        </div>
+                        :
+                        null
+                }
+
+            </div>
+        )
+    }
 }
 
+User.defaultProps = {
+    name: "-",
+    mail: "-",
+    phone: "-"
+}
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    mail: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired
+}
 
 export default User;
