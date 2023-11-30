460d47fac46c5e6fa110b18891112701b6acb571
OYENAL <OYENAL@DESKTOP-9PDS8TP>
2023-11-30 22:13:10 +0300

    PropTypes Default Props, Passing variable compone

---
+++ src/components/User.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+            <input type='text'></input>
+            <button>Gönder</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+export default User;
