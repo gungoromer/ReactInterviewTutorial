a899d64e3c4ed15f9a2bd45e3bb61245a22f4d90
Ömer GÜNGÖR <omer.gungor@wsrlife.com>
2023-12-01 10:09:24 +0300

    pose animation added.

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,34 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import posed from "react-pose";
+
+const Animation = posed.div({
+  visible: {
+    opacity: 1,
+    applyAtStart: {
+      display: "block",
+    },
+  },
+  hidden: {
+    opacity: 0,
+    applyAtEnd: {
+      display: "none",
+    },
+  },
+});
 
 class AddUser extends Component {
-    render() {
-        return (
-            <div className='col-8 mb-4'>
-                <div className='card'>
-                    <div className='card-header'>
-                        <h4>Add User Form</h4>
-                    </div>
-                    <div className='card-body'>
-                        <form>
-                            <div className='form-group'>
-                                <label>Ad</label>
-                                <input type='text' className='form-control' id="name"></input>
-                            </div>
-                            <div className='form-group'>
-                                <label>Mail</label>
-                                <input type='mail' className='form-control' id="mail"></input>
-                            </div>
-                            <div className='form-group'>
-                                <label>Phone</label>
-                                <input type='text' className='form-control' id="phone"></input>
-                            </div>
-                            <button type='submit' className='btn btn-primary'>Submit</button>
-                        </form>
-                    </div>
-                </div>
+  state = {
+    visible: true,
+  };
+  changeVisibility = (e) => {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  };
+  render() {
+    const { visible } = this.state;
+    return (
+      <div className="col-8 mb-4">
+        <button
+          onClick={this.changeVisibility}
+          className="btn btn-primary btn-block"
+        >
+          {visible ? "Hide Form" : "Show Form"}
+        </button>
+        <Animation pose={visible ? "visible" : "hidden"}>
+          <div className="card">
+            <div className="card-header">
+              <h4>Add User Form</h4>
             </div>
-        )
-    }
+            <div className="card-body">
+              <form>
+                <div className="form-group">
+                  <label>Ad</label>
+                  <input type="text" className="form-control" id="name"></input>
+                </div>
+                <div className="form-group">
+                  <label>Mail</label>
+                  <input type="mail" className="form-control" id="mail"></input>
+                </div>
+                <div className="form-group">
+                  <label>Phone</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="phone"
+                  ></input>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </Animation>
+      </div>
+    );
+  }
 }
 
 export default AddUser;
--- src/components/User.js
+++ src/components/User.js
@@ -1,77 +1,77 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import UserConsumer from '../context';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import UserConsumer from "../context";
 
 class User extends Component {
-    state = {
-        isVisible: true
-    }
+  state = {
+    isVisible: true,
+  };
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
+  onClickEvent(number, e) {
+    this.setState({
+      isVisible: !this.state.isVisible,
+    });
+  }
+  onDeleteUser(dispatch, e) {
+    const { id } = this.props;
+    console.log(this.props);
+    dispatch({
+      type: "DELETE_USER",
+      payload: id,
+    });
+  }
 
-    onClickEvent(number, e) {
-        this.setState({
-            isVisible: !this.state.isVisible
-        })
-    }
-    onDeleteUser(dispatch, e) {
-        const { id } = this.props;
-        console.log(this.props);
-        dispatch({
-            type: "DELETE_USER",
-            payload: id
-        });
-    }
+  render() {
+    const { id, name, mail, phone } = this.props;
+    const { isVisible } = this.state;
 
-    render() {
-
-        const { id, name, mail, phone } = this.props;
-        const { isVisible } = this.state;
-
-        return (
-            <UserConsumer>
-                {
-                    value => {
-                        const { dispatch } = value;
-                        return (
-                            <div className='card mb-5'>
-                                <div className='card-header' onClick={this.onClickEvent.bind(this, 34)}>
-                                    {name}-{id}
-                                </div>
-                                {
-                                    isVisible ?
-                                        <div className='card-body'>
-                                            <p>{mail}</p>
-                                            <p>{phone}</p>
-                                            <button className='btn btn-danger btn-sm' onClick={this.onDeleteUser.bind(this, dispatch)}>Sil</button>
-                                        </div>
-                                        :
-                                        null
-                                }
-
-                            </div>
-                        )
-                    }
-                }
-            </UserConsumer>
-        )
-    }
+    return (
+      <UserConsumer>
+        {(value) => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-5">
+              <div
+                className="card-header"
+                onClick={this.onClickEvent.bind(this, 34)}
+              >
+                {name}-{id}
+              </div>
+              {isVisible ? (
+                <div className="card-body">
+                  <p>{mail}</p>
+                  <p>{phone}</p>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={this.onDeleteUser.bind(this, dispatch)}
+                  >
+                    Sil
+                  </button>
+                </div>
+              ) : null}
+            </div>
+          );
+        }}
+      </UserConsumer>
+    );
+  }
 }
 
 User.defaultProps = {
-    id: 0,
-    name: "-",
-    mail: "-",
-    phone: "-"
-}
+  id: 0,
+  name: "-",
+  mail: "-",
+  phone: "-",
+};
 User.propTypes = {
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    mail: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired
-}
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  mail: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+};
 
 export default User;
